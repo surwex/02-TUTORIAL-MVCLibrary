7a74d3856d68fc8bb49233cb205609342cf473f1
Filip Maretić <fimareti@vsite.hr>
2024-11-23 17:26:17 +0100

    Rijeseno

---
+++ MVCLibrary/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace MVCLibrary.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        public string Author { get; set; }
+    }
+}
